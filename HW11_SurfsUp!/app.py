d0bfdd323a04b9d6625bcac0cb3c5b84bc64c027
YinLing Wang <35980231+yinlingw@users.noreply.github.com>
2018-04-21 10:50:06 -0700

    Add files via upload

--- HW11_SurfsUp!/app.py
+++ HW11_SurfsUp!/app.py
@@ -12,16 +12,17 @@ from flask import Flask, jsonify
 
 #################################################
 # Database Setup
-#################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+##########################################
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
+
 # Save reference to the table
 Measurement = Base.classes.measurements
-Station=Base.classes.stations
+Station = Base.classes.stations
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -37,57 +38,71 @@ app = Flask(__name__)
 #################################################
 
 @app.route("/")
-def homepage():
-    return(
-        f"Hawaii Weather Repository API<br/>"
-        f"Routes:<br/>"
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Surfs Up Info!!!!<br/>"
+        f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/temp/start/end"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
-def percipitation():
+def precipitation():
 
-    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2017-04-01").all()
-    for item in data:
-        data = {}
-        data["date"]=Measurement.date
-        data["prcp"]=Measurement.prcp
-    return jsonify(data)
-    
+    results_prec = session.query(Measurement.date, Measurement.tobs).\
+    filter(func.strftime("%Y", Measurement.date) == "2017").all()
+
+    temperature_dict = {}
+    for temp in results_prec:
+        temperature_dict["date"] = temp.tobs
+
+    return jsonify(temperature_dict)
+
+
 @app.route("/api/v1.0/stations")
 def stations():
-   results = session.query(Station.station).all()
-   all_names = list(np.ravel(results))
-   return jsonify(all_names)
+
+    results_stations = session.query(Station.station).all()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results_stations))
+
+    return jsonify(all_stations)
+
+
 @app.route("/api/v1.0/tobs")
-def tobs():
+def tobs_monthly():
     results = session.query(Measurement.tobs).\
-    filter(Measurement.date >= "2017-04-01").\
-    filter(Measurement.station == "USC00519281").all()
+        filter(Measurement.station == "USC00519281").\
+        filter(Measurement.date >= "2017-04-01").all()
 
-    temps = list(np.ravel(results))
-    return jsonify(temps)
+    temperatures = list(np.ravel(results))
+    return jsonify(temperatures)
+
+
 @app.route("/api/v1.0/<start>")
-def strtonly(start):
-    canonicalized = start.replace(" ", "")
-    results = session.query(func.avg(Measurement.tobs),func.min(Measurement.tobs),func.max(Measurement.tobs)).\
+def start_only(start):
+
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
     filter(Measurement.date >= start).all()
+
     new_data = list(np.ravel(results))
+
     return jsonify(new_data)
+
 @app.route("/api/v1.0/<start>/<end>")
-def startend(start, end):
-    new_start = start.replace(" ", "")
-    new_end = end.replace(" ", "")
-    results = session.query(func.avg(Measurement.tobs),func.min(Measurement.tobs),func.max(Measurement.tobs)).\
-    filter(Measurement.date >= start).\
-    filter(Measurement.date <= end).all()
-    new_data = list(np.ravel(results))
-    
-    return jsonify(new_data)
+def start_and_end(start, end):
 
+    results_calc = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+    filter(Measurement.date >= start, Measurement.date <= end).all()
 
-if __name__ == "__main__":
+    start_and_end_data = list(np.ravel(results))
+
+    return jsonify(start_and_end_data)
+
+if __name__ == '__main__':
     app.run(debug=True)
